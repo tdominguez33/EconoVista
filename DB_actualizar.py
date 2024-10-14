260178462f6366a17753006026ba7907d0f607bd
Tomás Dominguez <tdominguez33@outlook.com>
2024-10-14 16:40:08 -0300

    Mejorado proceso de actualización

--- DB_actualizar.py
+++ DB_actualizar.py
@@ -1,13 +1,23 @@
 # Archivo utilizado para actualizar la base de datos con los datos de como máximo UN AÑO atrás
-
+from funcionesDB import *
 import sqlite3
-from funcionesDB import actualizarDB
+import json
 
 # Conexión a la base de datos
 conn = sqlite3.connect('variables.db')
 cursor = conn.cursor()
 
-actualizarDB(cursor, conn)
+# Abrimos el archivo JSON que contiene las variables a obtener
+with open('variables.json', encoding='utf-8') as f:
+    archivo = json.load(f)
+
+# Insertar datos en la tabla DATA
+guardarVariables(archivo, cursor, conn)
+
+# Busca en la DB los ID's de las variables que tenemos
+listaVariables = crearListaVariables(cursor)
+
+actualizarVariables(listaVariables, cursor, conn)
 
 # Cierra la conexión a la base de datos
 conn.close()
